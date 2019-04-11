ce2778762a1d129f1fa536e2f2f32d9484895569
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-04-10 17:34:59 -0700

    Make Rails Lite

---
+++ skeleton/lib/controller_base.rb
@@ -0,0 +1,73 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require 'byebug'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    if !already_built_response?
+      @res.status = 302
+      @res.location = url
+    else
+      raise "Can't render and/or redirect twice"
+    end
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+
+    if !already_built_response?
+      @res.write(content)
+      @res['Content-Type'] = content_type
+      @already_built_response = true
+      @res.finish
+    else
+      raise "Can't render and/or redirect twice"
+    end
+    
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+
+    debugger
+
+    #localhost/cats/
+    #views/cats_controller/new.html.erb
+
+    
+
+
+    "views#{@req.path}_controller/#{template_name}.html.erb"
+    
+
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
+
